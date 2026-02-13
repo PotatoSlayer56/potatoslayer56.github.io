a286e2464f52c8ebfa2d679645dcec5abfa9002f
joshua-congreve <joshcongreve56@gmail.com>
2026-02-13 14:08:18 +1100

    added python projects to projects page

--- javascript/toggleProjects.js
+++ javascript/toggleProjects.js
@@ -1,8 +1,16 @@
 const modsSection = document.getElementById("mods");
 const modsToggle = document.getElementById("toggleMods")
 
+const pythonProjectsSection = document.getElementById("pythonProjects")
+const pythonProjectsToggle = document.getElementById("togglePythonProjects")
+
 modsSection.hidden = true;
+pythonProjectsSection.hidden = true;
 
 modsToggle.addEventListener("click", function() {
   modsSection.hidden = !modsSection.hidden;
+});
+
+pythonProjectsToggle.addEventListener("click", function() {
+  pythonProjectsSection.hidden = !pythonProjectsSection.hidden;
 });
